ebae40862281cdca730b144ac74a178123baa78d
andrew <andrew.gatt@springltd.co>
2018-03-25 19:05:17 +0100

    Fixed up nested promises, I'm new to this and best practises were hard to come by. Added retry facility, SuperHub seems to fail the first log in attempt, even though the password input name is correct and with a 500mS delay. Usually a second attempt will go through straight away, so I recommend this value is set to 1. Changed html to reflect the node properties.

--- superhub2-reboot.js
+++ superhub2-reboot.js
@@ -3,106 +3,114 @@ module.exports = function(RED) {
     const needle = require('needle')
     const cheerio = require('cheerio')
 
+    const TYPE_TEST = "test";
+    const TYPE_REBOOT = "reboot";
+
+    class RebootError extends Error{}
+    class RebootTestComplete extends Error{}
+
     function SuperHub2RebootNode(config) {
         RED.nodes.createNode(this, config);
         this.baseAddress = "http://" + config.ipAddress;
         this.password = config.password;
+        this.retries = config.retries;
         let node = this;
+        node.retryCount = 0;
 
-        function reboot(test) {
-            needle('get', node.baseAddress)
-            .then(response => {
-    
-                // first get password name attribute (it is randomly generated so we have to scrape the page for it)
-                let passwordName = _getPasswordName(response);
-                if (passwordName === null) {
-                    _failed("Failed to scrape password name");
-                    return;
-                }
-    
-                // next post the password using the scraped name
-                needle('post', node.baseAddress + '/cgi-bin/VmLoginCgi', passwordName + '=' + node.password, {'Content-Type': 'application/x-www-form-urlencoded'})
-                .then(response => {
-                    // we have to get the reboot page so we can scrape the randomly generated
-                    needle('get', node.baseAddress + '/VmRgRebootRestoreDevice.html')
-                    .then(response => {
-                        let rebootFormName = _getRebootFormName(response);
-                        if (rebootFormName === null) {
-                            _failed("Failed to scrape password name");
-                            return;
-                        }
-
-                        if (test === false) {
-                            // next post to cgi reboot form
-                            needle( 'post',
-                                    node.baseAddress + '/cgi-bin/VmRgRebootResetDeviceCfgCgi',
-                                    'VMRebootResetChangeCache=1&' + rebootFormName + '=0',
-                                    {'Content-Type': 'application/x-www-form-urlencoded'})
-                            .then(response => {
-                                _succeeded();
-                            })
-                            .catch(err => {
-                                _failed('Failed to post to reboot form ' + err);
-                            });
-                        }
-                        else {
-                            _succeeded();
-                        }
-                    })
-                    .catch(err => {
-                        _failed('Failed to get reboot page ' + err);
-                    });
-                })
-                .catch(err => {
-                    _failed('Failed to post password login ' + err);
-                });
+        function reboot() {
+            // first get password name attribute (it is randomly generated so we have to scrape the page for it
+            _getHomePage()
+            .then(_delay(500))
+            .then(response => _getPasswordName(response))
+            // post that to the login CGI
+            .then(passwordName => _postLogin(passwordName))
+            // we have to get the reboot page so we can scrape the randomly generated form name
+            .then(_getRebootPage)
+            .then(response => _getRebootFormName(response))
+             // next post to cgi reboot form
+            .then(() => {
+                node.send({ payload: true, type: TYPE_REBOOT });
             })
             .catch(err => {
-                _failed('Failed to get homepage ' + err);
+                if (err instanceof RebootTestComplete) {
+                    node.send({ payload: true, type: TYPE_TEST });
+                }
+                else {
+                    node.error(err);
+
+                    if (node.retryCount < node.retries) {
+                        reboot();
+                        node.retryCount++;
+                    }
+                    else {
+                        node.send({ payload: false, type: TYPE_REBOOT });
+                    }
+                }
             });
         }
 
-        function _succeeded() {
-            var msg = { payload: true };
-            try { node.send(msg); }
-            catch(e) {}
+        function _delay(ms) {
+            return function(x) {
+                return new Promise(resolve => setTimeout(() => resolve(x), ms));
+            };
         }
-    
-        function _failed(message) {
-            node.error(message);
-            var msg = { payload: false };
-            try { node.send(msg); }
-            catch(e) {}
+
+        function _getHomePage() {
+            return needle('get', node.baseAddress + '/VmLogin.html').catch(err => { throw new RebootError("Failed to get home page " + err) });
+        }
+
+        function _getPasswordName(response) {
+            let $ = cheerio.load(response.body);
+            let passwordName = $('#password').attr('name');
+            if ((typeof passwordName === "string") && (passwordName.length > 0)) {
+                return passwordName;
+            }
+            throw new RebootError("Failed to scrape password name");
+        }
+
+        function _postLogin(passwordName) {
+            return needle('post',
+                 node.baseAddress + '/cgi-bin/VmLoginCgi',
+                 passwordName + '=' + node.password,
+                 {'Content-Type': 'application/x-www-form-urlencoded'})
+                .catch(err => { throw new RebootError("Failed to post login form " + err) });
+        }
+
+        function _getRebootPage() {
+            return needle('get', node.baseAddress + '/VmRgRebootRestoreDevice.html').catch(err => { throw new RebootError("Failed to get reboot page " + err) });
         }
 
         function _getRebootFormName(response) {
             let $ = cheerio.load(response.body);
             let rebootFormName = $('#main').find('input[type="hidden"]').next().attr('name');
-    
             if ((typeof rebootFormName === "string") && (rebootFormName.length > 0)) {
-                return rebootFormName;
+                if (node.test === true) {
+                    throw new RebootTestComplete;
+                }
+                else {
+                    return rebootFormName;
+                }
             }
-            return null;
+            throw new RebootError("Failed to scrape reboot form name " + response.body);
         }
-    
-        function _getPasswordName(response) {
-            let $ = cheerio.load(response.body);
-            let passwordName = $('#password').attr('name');
-    
-            if ((typeof passwordName === "string") && (passwordName.length > 0)) {
-                return passwordName;
-            }
-            return null;
+
+        function _postRebootForm(rebootFormName) {
+            return needle('post',
+                           node.baseAddress + '/cgi-bin/VmRgRebootResetDeviceCfgCgi',
+                           'VMRebootResetChangeCache=1&' + rebootFormName + '=0',
+                           {'Content-Type': 'application/x-www-form-urlencoded'})
+            .catch(err => { throw new RebootError("Failed to post reboot form " + err) });
         }
 
         node.on('input', function(msg) {
-            if (msg.payload === "test") {
-                node.warn("Testing superhub2 at " + config.ipAddress);
-                reboot(true);
+            node.test = true;
+
+            if (msg.payload > 0) {
+                node.test = false;
+                reboot();
             }
-            else if (msg.payload > 0) {
-                node.log("Rebooting superhub2 at " + config.ipAddress);
-                reboot(false);
+            else if (msg.payload === "test") {
+                reboot();
             }
         });
     }
